1eff3d746035411a7f021debdf5a1b6a5ad68689
Yunyidi Han <yunyidi.han@gmail.com>
2020-09-01 10:51:57 -0700

    hiding key

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const axios = require("axios");
 const { response } = require("express");
 const server = express();
 
@@ -11,6 +13,16 @@ server.listen(PORT, () => {
 
 server.get("/", (req, res) => {
   res.send("Welcome Home");
+});
+
+server.get("/movies", (req, res) => {
+  axios
+    .get(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.OMDB_API_KEY}`
+    )
+    .then((result) => {
+      res.send(result);
+    });
 });
 
 server.get("/github", (req, res) => {
